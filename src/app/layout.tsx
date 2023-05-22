48be55cd7c6c4df881f1fec36acb121b9b2f2332
Matheus Mastelini <matheusmastelini@gmail.com>
2023-05-21 23:37:15 -0300

    update: Finishing memory creation and list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-x-visible overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
